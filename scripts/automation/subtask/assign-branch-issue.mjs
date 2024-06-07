fc213aa8de75be49a77d9e5080621b6af8aca785
Sebastian Claros <sclaros@gmail.com>
2024-06-07 17:10:04 -0300

    fix

--- scripts/automation/subtask/assign-branch-issue.mjs
+++ scripts/automation/subtask/assign-branch-issue.mjs
@@ -1,4 +1,4 @@
-import {assignBranchToIssue} from "./github.mjs";
+import {assignBranchToIssue} from "./github-graphql.mjs";
 
 const issueNumber = process.argv[2];
 const branch  = process.argv[3];
