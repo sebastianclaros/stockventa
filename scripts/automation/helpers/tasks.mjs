703e96081cd6d1c8747ccfa7c3bfd24e9a7389d8
Sebastian Claros <sclaros@gmail.com>
2024-07-11 12:11:22 -0300

    fix

--- scripts/automation/helpers/tasks.mjs
+++ scripts/automation/helpers/tasks.mjs
@@ -34,14 +34,17 @@ function getTask(taskName, folder) {
 
 function isCriteriaMet(criteria) {
   if ( !criteria ) {
+    console.log('sin criterio'); 
     return true;
   }
   const { field, value} = criteria;
   // si no viene valor solo verifica que tenga no este vacio
   if ( !value ) {
+    console.log(value); 
     return context[field] ;
-  } 
+  }
   const result = context[field] == value;
+  console.log(field, value, result); 
   return result;
 }
 
