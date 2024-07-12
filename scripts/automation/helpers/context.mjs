40e3aedd9719e964595d699277fd3abe87424247
Sebastian Claros <sclaros@gmail.com>
2024-07-11 22:22:28 -0300

    fix

--- scripts/automation/helpers/context.mjs
+++ scripts/automation/helpers/context.mjs
@@ -118,7 +118,8 @@ class Context {
     }
     setNewBranchName() {
         this.newBranchName =  this.branchNameFromIssue(this.newIssueType, this.newIssueNumber );
-        this.existNewBranch =  executeShell(`git show-ref refs/heads/${this.newBranchName}`) ? true : false;        
+        const salida =  executeShell(`git show-ref refs/heads/${this.newBranchName}`);
+        this.existNewBranch = (salida.includes(this.newBranchName));        
     }
     async askFornewBranchName() { 
         if ( !this.newBranchName ) {
